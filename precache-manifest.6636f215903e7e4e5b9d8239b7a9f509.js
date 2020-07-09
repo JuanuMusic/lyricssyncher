f0e934fb0ef5185b5fa0fd68784a865eb66e1e3f
Juan Manuel Haedo <juanu@juanu.com.ar>
2020-07-08 21:29:41 -0300

    Updates

---
+++ precache-manifest.6636f215903e7e4e5b9d8239b7a9f509.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99667b6a66b407e63a96b1ccdb129809",
+    "url": "/lyricssyncher/index.html"
+  },
+  {
+    "revision": "ff88cb8b401623d4852e",
+    "url": "/lyricssyncher/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1ca1428725310e7e5b5d",
+    "url": "/lyricssyncher/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ff88cb8b401623d4852e",
+    "url": "/lyricssyncher/static/js/2.b4fd4eea.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/lyricssyncher/static/js/2.b4fd4eea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ca1428725310e7e5b5d",
+    "url": "/lyricssyncher/static/js/main.4d4f6037.chunk.js"
+  },
+  {
+    "revision": "adc7077065177a978eb2",
+    "url": "/lyricssyncher/static/js/runtime-main.17c6bc25.js"
+  }
+]);
